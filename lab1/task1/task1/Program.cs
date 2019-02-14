7c0384b5e5240b84ba74069ae022afd3df90ff9d
e1dar7 <47219770+e1dar7@users.noreply.github.com>
2019-02-14 16:53:22 +0300

    Add files via upload

---
+++ lab1/task1/task1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());//Вводим через командную строку
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int cnt = 0;
+            bool status = false;        //status=можно представить что сейчас каждый член массива держит свой флаг(false)
+                                      // и если он не prime то его флаг станет true , и мы выведем те члены массива
+                                      //у кторых флаг false
+            for (int i = 0; i < n; i++)
+            {
+                status = false;
+                if (a[i] <= 1) continue;    //если член массива меньше или равен еднице то их не расматриваем
+                else
+                    for (int j = 2; j < a[i]; j++)
+                        if (a[i] % j == 0) status = true;     //если член массива не prime то вот тут он станет true
+                if (!status) cnt++;                    //считаем сколько у нас prime-ов
+            }
+
+            Console.WriteLine(cnt);
+
+            for (int i = 0; i < n; i++)
+            {
+                status= false;
+                if (a[i] <= 1) continue;
+                else
+                    for (int j = 2; j < a[i]; j++)
+                        if (a[i] % j == 0) status = true;
+                if (status==false) Console.Write(a[i] + " ");     //выводим все prime цифры
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab1/task3/task3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());  //Размер массива
+
+            int[] a = new int[n];           //Новый массив
+            for (int i = 0; i < n; i++)    //Пробегаемся по массиву
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());           //Ввод элементов массива через консоль
+            }
+            Console.WriteLine("Array: ");
+            Console.WriteLine(n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " ");  // Элементы массива
+            }
+            Console.WriteLine();
+            Console.WriteLine("Duplicates:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + " " + a[i] + " ");         //Дублируем элементы массива
+            }
+
+        }
+    }
+}
